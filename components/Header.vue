18e896e1c34f36561cd3dde0e8a749169f822b65
Joseph Ruano <seph.developer@gmail.com>
2021-02-19 22:47:58 -0600

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="flex">
+    <div class="flex items-center py-8 px-4">
+      <div class="w-8 h-8 mr-14">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+          />
+        </svg>
+      </div>
+      <div>
+        <a class="font-bold" href="mailto:hi@jruano.dev">hi@jruano.dev</a>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container mx-auto min-h-screen">
+    <Header />
+    <div class="flex items-center justify-center flex-col mt-28 pb-28">
+      <h1 class="text-2xl font-bold">Hi, I'm Joseph Ruano 🤘</h1>
+      <h2 class="mt-14 text-center text-6xl font-bold">
+        I <span class="highlighted">build</span> custom WordPress <br />themes
+        and plugins
+      </h2>
+      <div class="w-3/5 text-center mt-12 text-2xl">
+        <p>A <span class="font-bold">Web Developer</span> in WI.</p>
+        <p>
+          <span class="font-bold">I'm skilled in:</span> building and
+          customizing websites, creating custom plugins, website maintenance and
+          support, website optimization, fixing crashed and broken websites.
+        </p>
+        <div class="mt-12">
+          <a
+            class="px-6 py-4 bg-black border border-black hover:bg-white hover:text-black text-white text-base rounded-full"
+            href="mailto:hi@jruano.dev"
+            >CONTACT ME</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.highlighted {
+  position: relative;
+  z-index: 0;
+}
+
+.highlighted:before {
+  content: '';
+  width: 100%;
+  height: 12px;
+  bottom: 6px;
+  position: absolute;
+  background: #75e6da;
+  z-index: -1;
+}
+</style>
