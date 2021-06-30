00090b7ba5bec820f0f369eb8da2e3de6be20db3
Joseph Ruano <seph1603@gmail.com>
2021-06-29 22:37:47 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,10 +15,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-    ],
-    meta: [
-      { name: 'google-site-verification' },
-      { content: 'GkwCJy7emCXREtMiycMFMWt4LB53ZZT7MrD66Zq_8wc' }
+      { name: 'google-site-verification', content: 'GkwCJy7emCXREtMiycMFMWt4LB53ZZT7MrD66Zq_8wc' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
